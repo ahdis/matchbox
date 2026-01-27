e7cfafd04e16f0305e1252963a3f2bdc699ca05e
yoliyu <90109571+yoliyu@users.noreply.github.com>
2026-01-27 11:44:00 +0000

    server: rebuild the Angular GUI resources

---
+++ matchbox-server/src/main/resources/static/browser/chunk-7TGKTLXX.js
@@ -0,0 +1,1 @@
+import{Tc as a,Uc as b}from"./chunk-2HSRJ5V7.js";import"./chunk-TSRGIXR5.js";export{b as HighlightLineNumbers,a as activateLineNumbers};
--- matchbox-server/src/main/resources/static/browser/chunk-DEDHGZJT.js
+++
@@ -1,1 +0,0 @@
-import{Tc as a,Uc as b}from"./chunk-JL4BMBRH.js";import"./chunk-TSRGIXR5.js";export{b as HighlightLineNumbers,a as activateLineNumbers};
