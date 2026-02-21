5b982c907b817b0cdcf9cf288d6f0d4f68a0413b
Quentin Ligier <quentin.ligier@bluewin.ch>
2026-02-21 22:23:03 +0100

    Refactor EngineSessionCache

--- matchbox-server/src/main/java/ch/ahdis/matchbox/CliContext.java
+++ matchbox-server/src/main/java/ch/ahdis/matchbox/CliContext.java
@@ -868,6 +868,10 @@ public class CliContext {
     return result;
   }
 
+  public String sessionId() {
+	 return String.valueOf(this.hashCode());
+  }
+
   @Override
   public String toString() {
     return "CliContext{" +
