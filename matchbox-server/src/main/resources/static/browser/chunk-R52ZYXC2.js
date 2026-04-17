b80e22d54fe672d5af7f00f101c1ae157bc7fcf4
oliveregger <2414516+oliveregger@users.noreply.github.com>
2026-04-17 12:14:57 +0000

    server: rebuild the Angular GUI resources

---
+++ matchbox-server/src/main/resources/static/browser/chunk-R52ZYXC2.js
@@ -0,0 +1,1 @@
+import{Yc as a,Zc as b}from"./chunk-EGXMXL2T.js";import"./chunk-TSRGIXR5.js";export{b as HighlightLineNumbers,a as activateLineNumbers};
--- matchbox-server/src/main/resources/static/browser/chunk-XTGOQUEQ.js
+++
@@ -1,1 +0,0 @@
-import{Yc as a,Zc as b}from"./chunk-EML3IYS2.js";import"./chunk-TSRGIXR5.js";export{b as HighlightLineNumbers,a as activateLineNumbers};
