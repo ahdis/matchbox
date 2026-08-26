0d73db28c4219744616342acd91f2dc63d9d3f09
Quentin Ligier <quentin.ligier@bluewin.ch>
2026-08-26 14:08:00 +0200

    Create DocumentCompositionCodesExtractor

--- matchbox-server/src/main/java/ca/uhn/fhir/jpa/dao/data/MbInstalledStructureDefinitionRepository.java
+++ matchbox-server/src/main/java/ca/uhn/fhir/jpa/dao/data/MbInstalledStructureDefinitionRepository.java
@@ -3,6 +3,7 @@ package ca.uhn.fhir.jpa.dao.data;
 import ca.uhn.fhir.jpa.model.entity.MbInstalledStructureDefinitionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,4 +16,9 @@ public interface MbInstalledStructureDefinitionRepository
 
   @Query("SELECT e FROM MbInstalledStructureDefinitionEntity e WHERE e.isValidatable = TRUE ORDER BY e.title ASC")
   List<MbInstalledStructureDefinitionEntity> findAllValidatable();
+  
+  @Query("SELECT CASE WHEN COUNT(e) > 0 THEN true ELSE false END FROM MbInstalledStructureDefinitionEntity e " +
+    "WHERE e.canonicalUrl = :canonical AND e.type = :type")
+  boolean existsByCanonicalAndType(@Param("canonical") final String canonical,
+                                   @Param("type") final String type);
 }
